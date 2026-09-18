7fea51d925878b6de609eb7a5fac34ebc594f777
Tham You Jie <yjtham03@gmail.com>
2026-09-18 17:34:00 +0800

    Filter task by string

--- src/main/java/se/edu/streamdemo/Main.java
+++ src/main/java/se/edu/streamdemo/Main.java
@@ -4,7 +4,11 @@ import se.edu.streamdemo.data.Datamanager;
 import se.edu.streamdemo.task.Deadline;
 import se.edu.streamdemo.task.Task;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
 
 public class Main {
 
@@ -18,9 +22,9 @@ public class Main {
 
         System.out.println("Printing deadlines ...");
         printDeadlines(tasksData);
-
+        printDeadlinesUsingStreams(tasksData);
         System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
-
+        ArrayList<Task> filteredList = filterTaskByString(tasksData,"10");
     }
 
     private static int countDeadlines(ArrayList<Task> tasksData) {
@@ -46,5 +50,16 @@ public class Main {
             }
         }
     }
+    public static void printDeadlinesUsingStreams(ArrayList<Task> tasks){
+        System.out.println("Using Streams ....");
+        tasks.stream().filter((Task t)-> t instanceof Deadline)
+                .sorted((Task t1,Task t2) ->t1.getDescription().compareToIgnoreCase(t2.getDescription()))
+                .forEach(System.out::println);
+    }
 
+    public static ArrayList<Task> filterTaskByString(ArrayList<Task> tasks, String filterString){
+        return (ArrayList<Task>) tasks.stream()
+                .filter(t -> t.getDescription().contains(filterString))
+                .collect(toList());
+    }
 }
